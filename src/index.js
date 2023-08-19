b12c8200a725e2e6a5f911d67b4f91bfef3eb08c
GarikMSK <igor@nbma.ru>
2023-08-20 01:10:12 +0300

    init: done towels sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 
 // You should implement your task here.
+/*
+const matrix = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
 
-module.exports = function towelSort (matrix) {
-  return [];
+let towelsArray = []
+let matrix2 = []
+for (let i = 0; i < matrix.length; i++) {
+  console.log(matrix[i]);
+  
+  if (i%2) {matrix2 = matrix2.concat(matrix[i].reverse())}
+  else {matrix2 = matrix2.concat(matrix[i])}
+  
+
+
+  
 }
+console.log(matrix2);
+*/
+module.exports = function towelSort (matrix) {
+  let matrix2 = []
+  if(!matrix) return matrix2;
+  else {
+         for (let i = 0; i < matrix.length; i++) 
+          {
+            if (i%2) {matrix2 = matrix2.concat(matrix[i].reverse())}
+            else {matrix2 = matrix2.concat(matrix[i])}
+          }
+          return matrix2;
+        }
+}
+
